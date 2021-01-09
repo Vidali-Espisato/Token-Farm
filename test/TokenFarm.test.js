0d118d0a0b8a84e11aa63e7e90644f4db8683bed
Manish Roy <manish.11612939@lpu.in>
2021-01-09 22:28:37 +0530

    first deployment and test

---
+++ test/TokenFarm.test.js
@@ -0,0 +1,19 @@
+const { assert } = require("chai");
+
+const DappToken = artifacts.require("DappToken");
+const DaiToken = artifacts.require("DaiToken");
+const TokenFarm = artifacts.require("TokenFarm");
+
+require("chai")
+  .use(require("chai-as-promised"))
+  .should();
+
+contract("TokenFarm", (accounts) => {
+  describe("Mock DAI deployment", async () => {
+    it("has a name", async () => {
+      let daiToken = await DaiToken.new();
+      const name = await daiToken.name();
+      assert.equal(name, "Mock DAI Token");
+    });
+  });
+});
