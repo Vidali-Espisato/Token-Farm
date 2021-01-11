893be868e7535727c3b15618cd57986781437051
Manish Roy <manish.11612939@lpu.in>
2021-01-11 22:45:54 +0530

    issuing tokens

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -7,9 +7,43 @@ contract TokenFarm {
     string public name = "Dapp Token Farm";
     DappToken public dappToken;
     DaiToken public daiToken;
+    address public owner;
+    address[] public stakers;
+
+    mapping(address => bool) public isStaking;
+    mapping(address => bool) public hasStaked;
+    mapping(address => uint) public stakingBalance;
 
     constructor(DappToken _dappToken, DaiToken _daiToken) public {
         dappToken = _dappToken;
         daiToken = _daiToken;
+        owner = msg.sender;
+    }
+
+    function stakeTokens(uint _amount ) public {
+        require(_amount > 0, "Amount must be greater than 0.");
+
+        daiToken.transferFrom(msg.sender, address(this), _amount);
+        
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+        if (!hasStaked[msg.sender]) {
+            stakers.push(msg.sender);
+        }
+
+        isStaking[msg.sender] = true;
+        hasStaked[msg.sender] = true;
+    }
+
+    function issueTokens() public {
+        require(msg.sender == owner, "UnAuthorized");
+
+        for (uint i = 0; i < stakers.length; i++) {
+            address staker = stakers[i];
+            uint balance = stakingBalance[staker];
+
+            if (balance > 0) {
+                dappToken.transfer(staker, balance);
+            }
+        }
     }
 }
--- test/TokenFarm.test.js
+++ test/TokenFarm.test.js
@@ -8,12 +8,111 @@ require("chai")
   .use(require("chai-as-promised"))
   .should();
 
-contract("TokenFarm", (accounts) => {
+const tokens = (n) => {
+  return web3.utils.toWei(n, "Ether");
+};
+
+contract("TokenFarm", ([owner, investor]) => {
+  let daiToken, dappToken, tokenFarm;
+
+  before(async () => {
+    daiToken = await DaiToken.new();
+    dappToken = await DappToken.new();
+    tokenFarm = await TokenFarm.new(dappToken.address, daiToken.address);
+
+    await dappToken.transfer(tokenFarm.address, tokens("1000000"));
+    await daiToken.transfer(investor, tokens("100"), { from: owner });
+  });
+
   describe("Mock DAI deployment", async () => {
     it("has a name", async () => {
-      let daiToken = await DaiToken.new();
       const name = await daiToken.name();
       assert.equal(name, "Mock DAI Token");
+    });
+  });
+
+  describe("DApp Token deployment", async () => {
+    it("has a name", async () => {
+      const name = await dappToken.name();
+      assert.equal(name, "DApp Token");
+    });
+  });
+
+  describe("Token Farm deployment", async () => {
+    it("has a name", async () => {
+      const name = await tokenFarm.name();
+      assert.equal(name, "Dapp Token Farm");
+    });
+
+    it("has initial tokens", async () => {
+      let balance = await dappToken.balanceOf(tokenFarm.address);
+      assert.equal(balance.toString(), tokens("1000000"));
+    });
+  });
+
+  describe("Token Farm operations", async () => {
+    it("stakes mDai tokens approved by investors", async () => {
+      let balance = await daiToken.balanceOf(investor);
+      assert.equal(
+        balance.toString(),
+        tokens("100"),
+        "investor wallet mDai balance incorrect before staking"
+      );
+
+      await daiToken.approve(tokenFarm.address, tokens("50"), {
+        from: investor,
+      });
+      await tokenFarm.stakeTokens(tokens("50"), { from: investor });
+
+      balance = await daiToken.balanceOf(investor);
+      assert.equal(
+        balance.toString(),
+        tokens("50"),
+        "investor wallet mDai balance incorrect after staking"
+      );
+
+      let tfBalance = await daiToken.balanceOf(tokenFarm.address);
+      assert.equal(
+        tfBalance.toString(),
+        tokens("50"),
+        "token farm wallet mDai balance incorrect after staking"
+      );
+
+      await daiToken.approve(tokenFarm.address, tokens("30"), {
+        from: investor,
+      });
+      await tokenFarm.stakeTokens(tokens("30"), { from: investor });
+
+      let new_balance = await daiToken.balanceOf(investor);
+      assert.equal(
+        new_balance.toString(),
+        tokens("20"),
+        "investor wallet mDai balance incorrect after second stake"
+      );
+
+      let new_tfBalance = await daiToken.balanceOf(tokenFarm.address);
+      assert.equal(
+        new_tfBalance.toString(),
+        tokens("80"),
+        "token farm wallet mDai balance incorrect after second stake"
+      );
+
+      let result = await tokenFarm.isStaking(investor);
+      assert.equal(result, true);
+
+      result = await tokenFarm.stakingBalance(investor);
+      assert.equal(result.toString(), tokens("80"));
+
+      await tokenFarm.issueTokens({ from: owner });
+
+      result = await dappToken.balanceOf(investor);
+      assert.equal(
+        result.toString(),
+        tokens("80"),
+        "investor dApp wallet tokens incorrect"
+      );
+
+      await tokenFarm.issueTokens({ from: investor }).should.be.rejected;
     });
   });
 });
